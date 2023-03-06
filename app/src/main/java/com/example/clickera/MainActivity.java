9e4247b2994e4a063f409d1ca5d9364a66a292eb
Алексей Лисичников <alexochanel@gmail.com>
2023-03-06 21:47:58 +0300

    I doing this YEEEES

---
+++ app/src/main/java/com/example/clickera/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.clickera;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "StartActivity";
+
+    private Integer counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClickBtnAddDex(View view)
+    {
+        counter++;
+        TextView counterView = findViewById(R.id.txt_counter);
+        counterView.setText(counter.toString());
+    }
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("counter", counter);
+        Log.d(TAG, "onSaveInstanceState");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState != null &&
+                savedInstanceState.containsKey("counter")) {
+            counter = savedInstanceState.getInt("counter");
+            TextView counterView = findViewById(R.id.txt_counter);
+            counterView.setText(counter.toString());
+        }
+        Log.d(TAG, "onRestoreInstanceState");
+    }
+    private void resetUI() {
+        ((TextView) findViewById(R.id.txt_counter)).setText(counter.toString());
+        Log.d(TAG, "resetUI");
+    }
+}
